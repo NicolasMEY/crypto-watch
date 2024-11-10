c836a93b29c8c852b83f75df8838ec6382a581c8
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-11-10 16:31:05 +0100

    Add list.reducer

--- src/assets/components/Table.js
+++ src/assets/components/Table.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import TableLine from "./TableLine";
 import ToTop from "./ToTop";
 import { isStableCoin } from "./utils";
+import { useSelector } from "react-redux";
 
 const Table = ({ coinsData }) => {
   const [orderBy, setOrderBy] = useState("");
   const [rangeNumber, setRangeNumber] = useState(100);
   const showStable = useSelector((state) => state.stableReducer.showStable);
+  const showFavList = useSelector((state) => state.listReducer.showList);
 
   const tableHeader = [
     "Prix",
@@ -101,6 +103,16 @@ const Table = ({ coinsData }) => {
               if (isStableCoin(coin.symbol)) {
                 return coin;
               }
+            }
+          })
+          .filter((coin) => {
+            if (showFavList) {
+              let list = window.localStorage.coinList.split(",");
+              if (list.includes(coin.id)) {
+                return coin;
+              }
+            } else {
+              return coin;
             }
           })
           .sort((a, b) => {
--- src/assets/components/TableFilters.js
+++ src/assets/components/TableFilters.js
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from "react";
 import imgStarfull from "../imgs/star-full.svg";
 import { useDispatch } from "react-redux";
 import { setStableState } from "../../action/stable.action";
+import { setListDisplay } from "../../action/list.action";
 
 const TableFilters = () => {
   const [showStable, setShowStable] = useState(true);
+  const [showFavList, setShowFavlist] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(setStableState(showStable));
-  }, [showStable]);
+    dispatch(setListDisplay(showFavList));
+  }, [showStable, showFavList]);
 
   return (
     <div className="table-filters">
@@ -25,10 +28,16 @@ const TableFilters = () => {
             {showStable ? "Avec stable coin" : "Sans stable coin"}
           </label>
         </div>
-        <div className="no-list-btn">
+        <div
+          className={showFavList ? "no-list-btn" : "no-list-btn active"}
+          onClick={() => setShowFavlist(false)}
+        >
           <p>Aucune liste</p>
         </div>
-        <div className="fav-list">
+        <div
+          className={showFavList ? "fav-list active" : "fav-list"}
+          onClick={() => setShowFavlist(true)}
+        >
           <p>Liste des favoris</p>
           <img src={imgStarfull} alt="icon-star" />
         </div>
